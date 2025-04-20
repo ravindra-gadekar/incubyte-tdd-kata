ea909953bdd83be345d6dd7dbbd63c9e28c0f061
Ravindra Gadekar <ravindradgadekar@gmail.com>
2025-04-20 12:52:33 +0530

    put some meaningfull comments

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { add } from './stringCalculator';
 
 console.log("Testing String Calculator:");
+// test case: empty string
 console.log(`add("") = ${add('')}`);
+// test case: single number
 console.log(`add("1") = ${add('1')}`);
+// test case: any amount of numbers
 console.log(`add("1,2,3") = ${add('1,2,3')}`);
+// test case: negative numbers
 try {
   console.log(`add("1,-2,3,-4") = ${add('1,-2,3,-4')}`);
 } catch (e: any) {
   console.log(e.message);
 }
+// test case: numbers with different delimiters
 console.log(`add("1\n2,3") = ${add('1\n2,3')}`);
+// test case: custom delimiter
 console.log(`add("//;\n1;2") = ${add('//;\n1;2')}`);
