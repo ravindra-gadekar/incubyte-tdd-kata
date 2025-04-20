fb7e7ac7d86c1345bfdaa9bbf6a083d098586fbc
Ravindra Gadekar <ravindradgadekar@gmail.com>
2025-04-20 12:27:34 +0530

    add sixth test support different delimiters

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -21,4 +21,7 @@ describe('String Calculator', () => {
     expect(add('1\n2,3')).toBe(6);
   });
 
+  test('support different delimiters', () => {
+    expect(add('//;\n1;2')).toBe(3);
+  });
 });
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,12 @@
 export const add = (numbers: string): number => {
     if (numbers === '') return 0;
-    const nums = numbers.split(/[,\n]/).map(num => parseInt(num));
+    let delimiter: string | RegExp = /[,\n]/;
+    if (numbers.startsWith('//')) {
+      const delimiterEndIndex = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, delimiterEndIndex);
+      numbers = numbers.substring(delimiterEndIndex + 1);
+    }
+    
+    const nums = numbers.split(delimiter).map(num => parseInt(num));
     return nums.reduce((sum, num) => sum + num, 0);
   }
