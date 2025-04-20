ba6ce5222d160fb03733baa7c7cf150bf0cb4e8f
Ravindra Gadekar <ravindradgadekar@gmail.com>
2025-04-20 12:37:55 +0530

    test by passing negative number

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,8 @@ console.log("Testing String Calculator:");
 console.log(`add("") = ${add('')}`);
 console.log(`add("1") = ${add('1')}`);
 console.log(`add("1,2,3") = ${add('1,2,3')}`);
+try {
+  console.log(`add("1,-2,3,-4") = ${add('1,-2,3,-4')}`);
+} catch (e: any) {
+  console.log(e.message);
+}
