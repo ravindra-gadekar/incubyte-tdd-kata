94762871e724cdaf5a3c13f88858fb5c39fa66bd
Ravindra Gadekar <ravindradgadekar@gmail.com>
2025-04-20 12:25:34 +0530

    add fifth test handle new lines between numbers

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,5 @@
 export const add = (numbers: string): number => {
     if (numbers === '') return 0;
-    const nums = numbers.split(',').map(num => parseInt(num));
+    const nums = numbers.split(/[,\n]/).map(num => parseInt(num));
     return nums.reduce((sum, num) => sum + num, 0);
   }
