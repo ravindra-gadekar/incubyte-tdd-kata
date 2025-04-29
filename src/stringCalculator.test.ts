1a451fcc2b931bf9f6a14e99e3d5149b37f7f40a
Ravindra Gadekar <ravindradgadekar@gmail.com>
2025-04-29 15:44:55 +0530

    added new test case for custome delimator *

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -1,31 +1,37 @@
-import { add } from './stringCalculator';
+import { add } from "./stringCalculator";
 
-describe('String Calculator', () => {
-  test('empty string returns 0', () => {
-    expect(add('')).toBe(0);
-  });
-    
-  test('single number returns the number', () => {
-    expect(add('1')).toBe(1);
+describe("String Calculator", () => {
+  test("empty string returns 0", () => {
+    expect(add("")).toBe(0);
   });
 
-  test('two numbers comma delimited returns the sum', () => {
-    expect(add('1,5')).toBe(6);
+  test("single number returns the number", () => {
+    expect(add("1")).toBe(1);
   });
 
-  test('handle any amount of numbers', () => {
-    expect(add('1,2,3,4,5')).toBe(15);
+  test("two numbers comma delimited returns the sum", () => {
+    expect(add("1,5")).toBe(6);
   });
 
-  test('handle new lines between numbers', () => {
-    expect(add('1\n2,3')).toBe(6);
+  test("handle any amount of numbers", () => {
+    expect(add("1,2,3,4,5")).toBe(15);
   });
 
-  test('support different delimiters', () => {
-    expect(add('//;\n1;2')).toBe(3);
+  test("handle new lines between numbers", () => {
+    expect(add("1\n2,3")).toBe(6);
   });
 
-  test('negative numbers throw exception', () => {
-    expect(() => add('1,-2,3,-4')).toThrow('negative numbers not allowed -2,-4');
+  test("support different delimiters ;", () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });
+
+  test("support different delimiters *", () => {
+    expect(add("//*\n1*2")).toBe(2);
+  });
+
+  test("negative numbers throw exception", () => {
+    expect(() => add("1,-2,3,-4")).toThrow(
+      "negative numbers not allowed -2,-4"
+    );
   });
 });
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,18 +1,23 @@
 export const add = (numbers: string): number => {
-    if (numbers === '') return 0;
-    let delimiter: string | RegExp = /[,\n]/;
-    if (numbers.startsWith('//')) {
-      const delimiterEndIndex = numbers.indexOf('\n');
-      delimiter = numbers.substring(2, delimiterEndIndex);
-      numbers = numbers.substring(delimiterEndIndex + 1);
-    }
-    
-    const nums = numbers.split(delimiter).map(num => parseInt(num));
-    const negatives = nums.filter(num => num < 0);
-    
-    if (negatives.length > 0) {
-      throw new Error(`negative numbers not allowed ${negatives.join(',')}`);
-    }
-    
-    return nums.reduce((sum, num) => sum + num, 0);
+  let isStar = false;
+  if (numbers === "") return 0;
+  let delimiter: string | RegExp = /[,\n]/;
+  if (numbers.startsWith("//")) {
+    console.log("numbers", numbers);
+    const delimiterEndIndex = numbers.indexOf("\n");
+    delimiter = numbers.substring(2, delimiterEndIndex);
+    if (delimiter === "*") isStar = true;
+    numbers = numbers.substring(delimiterEndIndex + 1);
   }
+
+  const nums = numbers.split(delimiter).map((num) => parseInt(num));
+  const negatives = nums.filter((num) => num < 0);
+
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+  }
+
+  if (isStar) return nums.reduce((sum, num) => sum * num, 1);
+
+  return nums.reduce((sum, num) => sum + num, 0);
+};
